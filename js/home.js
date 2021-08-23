b936d54275ab3be8809744bdb44cc43a5bdb2a02
RimantasZiedelis <rimantas.ziedelis@yahoo.co.uk>
2021-08-23 23:53:36 +0300

    Git-conflict-prevention

--- js/home.js
+++ js/home.js
@@ -1,3 +1,42 @@
 // import JS modules
 
 // execute JS modules
+
+        /* Header componnents pradzia  */
+        /* Header componnents pabaiga  */
+      
+        /* Learn UX design pradzia  */
+        /* Learn UX design pabaiga  */
+      
+        /* Date / time pradzia  */
+        /* Data / time pabaiga  */
+            
+        /* What u learn pradzia  */
+        /* What u learn pabaiga  */
+            
+        /* Cource syllabus pradzia  */
+        /* Cource syllabus pabaiga  */
+            
+        /* Cource benefit pradzia  */
+        /* Cource benefit pabaiga  */
+            
+        /* Pricing pradzia  */
+        /* Pricing pabaiga  */
+          
+        /* Studients reviews pradzia  */
+        /* Studients reviews pabaiga  */
+            
+        /* Form pradzia  */
+        /* Form pabaiga  */
+            
+        /* Cource insructor pradzia  */
+        /* Cource insructor pabaiga  */
+            
+        /* Cource insructor pradzia  */
+        /* Cource insructor pabaiga  */
+            
+        /* Form pradzia  */
+        /* Form pabaiga  */
+           
+        /* Footer pradzia  */
+        /* Footer pabaiga  */
